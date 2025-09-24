765141e230d5ee7a08e1f4c6a5ecccd0b48665f6
Lukas Lopes Mussoi <lukasufpr@gmail.com>
2025-09-24 14:22:13 -0300

    docs: atualizar configuracoes e documentacao do projeto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   
   // Configuração Tailwind para usar nosso arquivo CSS
   tailwindcss: {
